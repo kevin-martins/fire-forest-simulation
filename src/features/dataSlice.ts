61f6fa5e38280bd5aca16cb2465f600929edba50
kevin-martins <kev.martinsvieira@gmail.com>
2024-03-07 21:57:25 +0100

    fix: move world and tiles config in the store and change to Typescript

---
+++ src/features/dataSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import TerrainConfigProps, { TerrainState } from '../models/terrainConfig';
+import WorldConfigProps from '../models/worldConfig';
+
+type InitialState = {
+  worldConfig: WorldConfigProps
+  terrainConfig: TerrainConfigProps
+}
+
+const initialState: InitialState = {
+  worldConfig: {
+    width: 10,
+    height: 10
+  },
+  terrainConfig: {
+    coordinates: [],
+    temperature: 20 ,
+    humidity: 0,
+    state: TerrainState.None
+  }
+};
+
+export const dataSlice = createSlice({
+  name: 'data',
+  initialState,
+  reducers: {
+    updateWorldConfig(state, action: PayloadAction<WorldConfigProps>) {
+      state.worldConfig = action.payload
+    },
+    updateTerrainConfig(state, action: PayloadAction<TerrainConfigProps>) {
+      state.terrainConfig = action.payload
+    }
+  }
+});
+
+export const {
+  updateWorldConfig,
+  updateTerrainConfig
+} = dataSlice.actions;
+
+export default dataSlice.reducer;
