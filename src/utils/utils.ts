2ab92d3aa367f20001a146b99c09585a0c678c31
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-08 16:31:05 +0100

    feat: add tile state to text

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,13 @@
+import { TileState } from "../models/terrainConfig";
+
 export const randomNumberRange = (min: number, max: number): number => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export const tileStateToText = (state: TileState) => {
+    switch(state) {
+        case 0: return 'Forest'
+        case 1: return 'Fire'
+        case 2: return 'Ash'
+    }
 }
