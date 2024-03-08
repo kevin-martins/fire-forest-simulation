6479f830ed064041fa9ed5032f5fe818a28ce3c4
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-08 19:44:42 +0100

    feat: add a function to convert enum to text

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,23 @@
+import SimulationState from "../models/simulationState";
 import { TileState } from "../models/terrainConfig";
 
 export const randomNumberRange = (min: number, max: number): number => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export const tileStateToText = (state: TileState) => {
-    switch(state) {
-        case 0: return 'Forest'
-        case 1: return 'Fire'
-        case 2: return 'Ash'
-    }
+  switch(state) {
+    case 0: return 'Forest'
+    case 1: return 'Fire'
+    case 2: return 'Ash'
+  }
+}
+
+export const simulationStateToText = (state: SimulationState) => {
+  switch(state) {
+    case 0: return 'Initialisation'
+    case 1: return 'Step by Step'
+    case 2: return 'Auto'
+    case 2: return 'Stop'
+  }
 }
