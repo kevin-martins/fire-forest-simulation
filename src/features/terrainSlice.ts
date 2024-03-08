aa7baa784fb6bd38f194adfbb7822714b1edc85a
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-08 19:46:01 +0100

    feat: add a function to build the terrain in async

--- src/features/terrainSlice.ts
+++ src/features/terrainSlice.ts
@@ -1,20 +1,54 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AsyncThunk, createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import TileConfigProps from '../models/terrainConfig';
 import SimulationState from '../models/simulationState';
 
 interface InitialState {
+  loading: boolean
   terrain: TileConfigProps[][]
   width: number
   height: number
   simulationState: SimulationState
+  error: string | null
+}
+
+const config = {
+  width: 10,
+  height: 10,
 }
 
 const initialState: InitialState = {
+  ...config,
+  loading: false,
   terrain: [],
-  width: 10,
-  height: 10,
   simulationState: SimulationState.Init,
+  error: null
 };
+
+export const generateTerrain = createAsyncThunk(
+  'terrain/generateTerrain',
+  async (payload: { width: number; height: number, tileConfig: TileConfigProps }) => {
+    const { width, height, tileConfig } = payload
+    const newTerrain: TileConfigProps[][] = []
+    
+    for (let i = 0; i < height; i++) {
+      const row: TileConfigProps[] = []
+      for (let j = 0; j < width; j++) {
+        const config: TileConfigProps = {
+          ...tileConfig,
+          coordinates: {
+            row: i + 1,
+            col: j + 1
+          },
+        }
+        row.push({ ...config })
+      }
+      newTerrain.push(row)
+    }
+    return newTerrain
+  }
+);
+
+
 
 export const terrainSlice = createSlice({
   name: 'terrainConfig',
@@ -32,7 +66,21 @@ export const terrainSlice = createSlice({
     setState(state, action: PayloadAction<SimulationState>) {
       state.simulationState = action.payload
     }
-  }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(generateTerrain.pending, (state) => {
+      state.loading = true
+      state.error = null
+    });
+    builder.addCase(generateTerrain.fulfilled, (state, action) => {
+      state.loading = false
+      state.terrain = action.payload
+    });
+    builder.addCase(generateTerrain.rejected, (state, action) => {
+      state.loading = false
+      state.error = action.error.message ?? 'An error occurred while generating terrain.'
+    });
+  },
 });
 
 export const {
