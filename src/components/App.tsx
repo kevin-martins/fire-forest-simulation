cf2a11c04c3d35f3634c83d15dfcfbb7fc0f3aca
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-08 16:57:10 +0100

    fix: clear App file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,87 +1,32 @@
-import { useEffect, useState } from "react";
-import TerrainConfigProps, { TerrainState } from "../models/terrainConfig";
-import { useSelector } from 'react-redux';
-import TerrainSquare from "./TerrainSquare";
+import { FaPlay, FaRegPauseCircle, FaStepForward,  } from "react-icons/fa";
+import Terrain from "./Terrain";
+import TerrainConfig from "./TerrainConfig";
+import TileConfig from "./TileConfig";
 
 const App = () => {
-  const [width, setWidth] = useState<number>(15)
-  const [height, setHeight] = useState<number>(15)
-  const terrainConfig = useSelector((state: any) => state.data.terrainConfig)
-  // const [terrainConfig, setTerrainConfig] = useState<TerrainConfigProps>(initialTerrainConfig)
-  const [terrain, setTerrain] = useState<TerrainConfigProps[][]>([]);
-
-  const handleWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setWidth(Number(event.target.value))
-  }
-
-  const handleHeightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setHeight(Number(event.target.value))
-  }
-
-  const handleTerrainGeneration = () => {
-    const newTerrain: TerrainConfigProps[][] = []
-    for (let i = 0; i < height; i++) {
-      const row: TerrainConfigProps[] = []
-      for (let j = 0; j < width; j++) {
-        const config: TerrainConfigProps = {
-          ...terrainConfig,
-          coordinates: [i, j],
-          temperature: 19,
-          humidity: 1
-        }
-        row.push({ ...config })
-      }
-      newTerrain.push(row)
-    }
-    setTerrain(newTerrain)
-  }
-
-  const handleTerrainConfigChanges = (cell: TerrainConfigProps) => {
-    const [row, col] = cell.coordinates
-    const newTerrain = terrain.slice();
-    // Update the specific cell with newValue
-    newTerrain[row] = newTerrain[row].slice();
-    newTerrain[row][col] = { ...cell, state: TerrainState.Fire };
-    // Update the state with the modified terrain array
-    setTerrain(newTerrain);
-  }
-
-  useEffect(() => {
-    handleTerrainGeneration()
-  },[])
-
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
       </header>
       <main className="bg-slate-800 h-screen">
-        {terrain.map(row => {
-          return (
-            <div className="flex flex-row">
-              {row.map(cell => {
-                return (
-                  <TerrainSquare {...cell} />
-                )
-              })}
-            </div>
-          )
-        })}
-        <div>
-          <h1>Terrain Configuration</h1>
+        <div className="container mx-auto">
+          <Terrain />
+          <div className="flex flex-row gap-5 place-content-center mt-3">
+            <button className="">
+              <FaPlay className=" text-white" />
+            </button>
+            <button className="scale-150">
+              <FaRegPauseCircle className=" text-white" />
+            </button>
+            <button className="">
+              <FaStepForward className=" text-white" />
+            </button>
+
+
+          </div>
+          <TerrainConfig />
+          <TileConfig />
         </div>
-        <label>
-          Width:
-          <input type="number" value={width} onChange={handleWidthChange} />
-        </label>
-        <br />
-        <label>
-          Height:
-          <input type="number" value={height} onChange={handleHeightChange} />
-        </label>
-        <br />
-        <button onClick={handleTerrainGeneration}>Apply Changes</button>
-        <p>Width: {width}</p>
-        <p>Height: {height}</p>
       </main>
     </div>
   );
