55a84e9072dafc464e822d7da8955acbd1c20e6d
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-10 13:48:29 +0100

    style: change tile config style

--- src/components/TileConfig.tsx
+++ src/components/TileConfig.tsx
@@ -1,17 +1,17 @@
-import { useSelector } from 'react-redux'
-import { RootState } from '../app/store'
-import { setBurningDuration, setHumidity, setState, setTemperature } from '../features/tileSlice'
-import InputNumber from './InputNumber'
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+import { setBurningDuration, setHumidity, setTemperature } from '../features/tileSlice';
+import InputNumber from './InputNumber';
 
 const TileConfig = () => {
-  const { temperature, humidity, state, burningDuration } = useSelector((state: RootState) => state.tileConfig)
+  const { temperature, humidity, burningDuration } = useSelector((state: RootState) => state.tileConfig)
 
   return (
-    <div className='grid grid-cols-1'>
-      <h1>Tile Configuration</h1>
-      <InputNumber title='Temperature' description='the temperature must be between -20 and 50°C' value={temperature} reduxFunction={setTemperature} />
-      <InputNumber title='Humidity' description='the humidity must be between 0 and 1' value={humidity} reduxFunction={setHumidity} step={0.1} />
-      <InputNumber title='Burning Duration' description='the time required for the tile to become ash after being on fire' value={burningDuration} reduxFunction={setBurningDuration} />
+    <div className='flex flex-col'>
+      <h1 className='text-white text-lg font-medium'>Tile Configuration</h1>
+      <InputNumber title='Temperature' description='the temperature must be between -20 and 50°C' value={temperature} reduxFunction={setTemperature} unit='°C' />
+      <InputNumber title='Humidity' description='the humidity must be between 0 and 100%' value={humidity} reduxFunction={setHumidity} unit='%' />
+      <InputNumber title='Burning Duration' description='the time required for the tile to become ash after being on fire' value={burningDuration} reduxFunction={setBurningDuration} unit='turn' />
     </div>
   )
 }
