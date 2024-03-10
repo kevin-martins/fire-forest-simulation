d8c389fe359f1a3e5bd60083ba6fcc9380013d16
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-10 12:42:30 +0100

    feat: add explanation dialogue data and component

---
+++ src/constants/explanationState.ts
@@ -0,0 +1,41 @@
+import GameState from "../models/gameState";
+
+export const explanationData = {
+  [GameState.Config]: {
+    title: 'Update Settings',
+    steps: [
+      '- Get more control by changing the propreties down bellow',
+      '- Rebuild the terrain if you did change something',
+      '- Follow your needs, no need to be shy !',
+      '- Click the button to continue'
+    ],
+    delay: 2
+  },
+  [GameState.Fire]: {
+    title: 'Add Fire on Tiles',
+    steps: [
+      "- Now it's time to add fire in the scene",
+      '- Hover a tile to preview the fire',
+      '- Click on it to apply the fire on it',
+      '- Click the button to continue'
+    ],
+    delay: 1.7
+  },
+  [GameState.BeforeStart]: {
+    title: 'Last Thing...',
+    steps: [
+      '- Click the start simulation'
+    ],
+    delay: .4
+  },
+  [GameState.End]: {
+    title: 'Simulation Completed',
+    steps: [
+      '- The simulation has ended',
+      '- Get more detail by checking the simulation stats'
+    ],
+    delay: .8
+  }
+}
+
+export default explanationData
