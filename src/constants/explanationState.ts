edad8ced51d4d986a8065f34579f1cf4a56088d7
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-12 19:01:43 +0100

    feat: change explanation data

--- src/constants/explanationState.ts
+++ src/constants/explanationState.ts
@@ -6,23 +6,23 @@ export const explanationData = {
     steps: [
       "- Get more control by changing propreties down bellow",
       "- After any changes, rebuild the terrain with the 'Build Terrain' button",
-      "- Ignite green tiles by clicking on it, click again to remove or use the 'Ignite' button",
+      "- Ignite green tiles by clicking on them, click again to remove or use the 'Ignite' button",
       "- Perfect ! Let's run the simulation now by clicking the 'Start' button"
     ]
   },
   [GameState.Running]: {
     title: 'Simulation is Running...',
     steps: [
-      "- Cool ! The simulation is now running",
-      "- Keep clicking to run the next simulation step"
+      "- The simulation is now running",
+      "- Keep clicking to run the next step"
     ]
   },
   [GameState.End]: {
     title: 'Simulation Completed',
     steps: [
       "- The simulation has ended",
-      "- Get more detail by checking the simulation stats",
-      "- To reset the terrain, just rebuild it with the 'Build Terrain' button !"
+      "- Get more detail by checking the simulation stats down the terrain",
+      "- To reset the simulation, just rebuild it with the 'Build Terrain' button"
     ]
   }
 }
