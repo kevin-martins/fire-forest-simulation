37547aa5ede687b1fc58fff5e82838e72b90b70c
Kevin Martins <kev.martinsvieira@gmail.com>
2024-03-12 18:02:55 +0100

    feat: change input style and link label with input

--- src/components/InputNumber.tsx
+++ src/components/InputNumber.tsx
@@ -1,8 +1,9 @@
-import { PayloadAction } from '@reduxjs/toolkit'
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import { PayloadAction } from '@reduxjs/toolkit';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 type Props = {
+  id: string
   title: string
   description: string
   value: number
@@ -11,61 +12,33 @@ type Props = {
   step?: number
 }
 
-const InputNumber = ({ title, description, value, reduxFunction, unit = '', step = 1 }: Props) => {
+const InputNumber = ({ id, title, description, value, reduxFunction, unit = '', step = 1 }: Props) => {
+  const [error, setError] = useState<boolean>(false)
   const dispatch = useDispatch()
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(reduxFunction(Number(event.target.value)))
   }
-  
-  const handleDecrease = () => {
-    dispatch(reduxFunction(value - step))
-  }
-
-  const handleIncrease = () => {
-    dispatch(reduxFunction(value + step))
-  }
 
   return (
-    <div className="pl-5">
-      <label htmlFor="quantity-input" className="block mb-2 text-md font-medium text-white">{title}:</label>
-      <p id="helper-text-explanation" className="mt-2 text-sm text-gray-400 block align-top">{description}</p>
-      <div className="relative flex items-center max-w-[9rem]">
-        <button
-          type="button"
-          id="decrement-button"
-          data-input-counter-decrement="quantity-input"
-          className="bg-gray-700 hover:bg-gray-600 border-gray-600 border rounded-s-lg p-3 h-10  focus:ring-gray-800 group"
-          onClick={handleDecrease}
-        >
-          <svg className="group-hover:scale-125 w-3 h-3 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
-          </svg>
-        </button>
+    <div>
+      <label htmlFor={`${id}-input`} className="block mb-px text-md font-medium text-white">
+        {title}:
+        <p id="helper-text-explanation" className="font-normal mt-1 text-sm text-gray-400 block align-top">{description}</p>
+      </label>
+      <div className="relative flex items-center max-w-[8rem]">
         <input
-          type="text"
-          id="quantity-input"
+          type="number"
+          id={`${id}-input`}
           data-input-counter
-          aria-describedby="helper-text-explanation"
-          className="h-10 text-center text-lg block w-full py-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white outline-none"
-          placeholder="999"
+          className="relative h-10 text-center text-lg block w-full py-2.5 bg-gray-700 border-gray-600 rounded text-white outline-none"
           value={value}
           onChange={handleChange}
           required
         />
-        <button
-          type="button"
-          id="increment-button"
-          data-input-counter-increment="quantity-input"
-          className="group bg-gray-700 hover:bg-gray-600 border-gray-600 border rounded-e-lg p-3 h-10 focus:ring-gray-700"
-          onClick={handleIncrease}
-        >
-          <svg className="group-hover:scale-125 w-3 h-3 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-          </svg>
-        </button>
+        <span className='absolute z-10 text-slate-100 left-1/2 px-2'>{unit}</span>
       </div>
-      {/* <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">Please select a 5 digit number from 0 to 9.</p> */}
+      {error && <p id="helper-text-explanation" className="mt-2 text-sm text-red-400">the value is out of bounds</p>}
     </div>
   )
 }
